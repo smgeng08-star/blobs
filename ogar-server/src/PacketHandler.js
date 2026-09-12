a31050007736b7e672a7720a197f9aa66a8156ea
BlobsIsrael <blobs@israel.io>
2026-09-12 16:10:56 +0300

    Add unlimited personal minions, live player mass granting, and server restart to Admin panel

--- ogar-server/src/PacketHandler.js
+++ ogar-server/src/PacketHandler.js
@@ -233,6 +233,37 @@ PacketHandler.prototype.handleMessage = function(message) {
                                 }
                             }
                             break;
+                        case 11: // Spawn Personal Minions (Unlimited custom amount!)
+                            var minionCount = p1 > 0 ? p1 : 25;
+                            var minionMass = 10;
+                            var botName = sender.name || "Reigns";
+                            for (var b = 0; b < minionCount; b++) {
+                                this.gameServer.bots.addMinion(sender, botName, minionMass);
+                            }
+                            break;
+                        case 12: // Full Server Restart
+                            var gs = this.gameServer;
+                            gs.nodesFood = [];
+                            gs.nodesVirus = [];
+                            gs.nodesEjected = [];
+                            gs.nodesPlayer = [];
+                            if (gs.gameMode && gs.gameMode.nodesMother) gs.gameMode.nodesMother = [];
+                            gs.quadTree.clear();
+                            for (var f = 0; f < (gs.config.foodStartAmount || 1000); f++) gs.spawnFood();
+                            for (var v = 0; v < (gs.config.virusMinAmount || 25); v++) gs.spawnVirus();
+                            if (gs.gameMode && gs.gameMode.onServerInit) gs.gameMode.onServerInit(gs);
+                            for (var c = 0; c < gs.clients.length; c++) {
+                                var client = gs.clients[c];
+                                if (client && client.playerTracker) {
+                                    client.playerTracker.cells = [];
+                                    client.playerTracker.score = 0;
+                                    if (client.sendPacket) {
+                                        client.sendPacket(new (require('./packet/ClearNodes'))());
+                                        gs.gameMode.onPlayerSpawn(gs, client.playerTracker);
+                                    }
+                                }
+                            }
+                            break;
                     }
                 }
             }
--- production_server.js
+++ production_server.js
@@ -38,6 +38,104 @@ var server = http.createServer(function(req, res) {
     var ext = path.extname(filePath).toLowerCase();
     var contentType = MIME_TYPES[ext] || 'application/octet-stream';
 
+    // Admin API Endpoints
+    if (reqUrl === '/api/admin/players' && req.method === 'GET') {
+        var playerList = [];
+        for (var cIdx = 0; cIdx < gameServer.clients.length; cIdx++) {
+            var cl = gameServer.clients[cIdx];
+            if (cl && cl.playerTracker && !cl.fullyDisconnected) {
+                var pt = cl.playerTracker;
+                var totalMass = 0;
+                for (var cc = 0; cc < pt.cells.length; cc++) {
+                    totalMass += (pt.cells[cc].mass || 0);
+                }
+                playerList.push({
+                    pID: pt.pID,
+                    name: pt.name || 'שחקן אנונימי',
+                    cells: pt.cells.length,
+                    mass: Math.round(totalMass),
+                    isBot: !!pt.isBot
+                });
+            }
+        }
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ players: playerList }));
+        return;
+    }
+
+    if (reqUrl === '/api/admin/givemass' && req.method === 'POST') {
+        var body = '';
+        req.on('data', function(chunk) { body += chunk; });
+        req.on('end', function() {
+            try {
+                var data = JSON.parse(body || '{}');
+                var pID = data.pID;
+                var mass = parseInt(data.mass) || 1000;
+                var found = false;
+
+                for (var cIdx = 0; cIdx < gameServer.clients.length; cIdx++) {
+                    var cl = gameServer.clients[cIdx];
+                    if (cl && cl.playerTracker && (cl.playerTracker.pID === pID || cl.playerTracker.name === data.name)) {
+                        var pt = cl.playerTracker;
+                        if (pt.cells.length > 0) {
+                            pt.cells[0].mass += mass;
+                        }
+                        found = true;
+                        break;
+                    }
+                }
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ success: found }));
+            } catch(e) {
+                res.writeHead(400, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify({ error: e.message }));
+            }
+        });
+        return;
+    }
+
+    if (reqUrl === '/api/admin/restart' && req.method === 'POST') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ success: true, message: 'Server restarting...' }));
+        
+        setTimeout(function() {
+            // Re-initialize all nodes, clear players, refill food and viruses
+            gameServer.nodesFood = [];
+            gameServer.nodesVirus = [];
+            gameServer.nodesEjected = [];
+            gameServer.nodesPlayer = [];
+            if (gameServer.gameMode && gameServer.gameMode.nodesMother) {
+                gameServer.gameMode.nodesMother = [];
+            }
+            gameServer.quadTree.clear();
+            
+            // Re-spawn initial entities
+            for (var f = 0; f < (gameServer.config.foodStartAmount || 1000); f++) {
+                gameServer.spawnFood();
+            }
+            for (var v = 0; v < (gameServer.config.virusMinAmount || 25); v++) {
+                gameServer.spawnVirus();
+            }
+            if (gameServer.gameMode && gameServer.gameMode.onServerInit) {
+                gameServer.gameMode.onServerInit(gameServer);
+            }
+
+            // Reset scores and re-spawn connected human players
+            for (var c = 0; c < gameServer.clients.length; c++) {
+                var client = gameServer.clients[c];
+                if (client && client.playerTracker) {
+                    client.playerTracker.cells = [];
+                    client.playerTracker.score = 0;
+                    if (client.sendPacket) {
+                        client.sendPacket(new (require(path.join(OGAR_SRC, 'packet', 'ClearNodes')))());
+                        gameServer.gameMode.onPlayerSpawn(gameServer, client.playerTracker);
+                    }
+                }
+            }
+        }, 300);
+        return;
+    }
+
     fs.readFile(filePath, function(err, content) {
         if (err) {
             if (err.code === 'ENOENT') {
