3746f9154d2638e47a77af0f6d1231120f9655b3
BlobsIsrael <blobs@israel.io>
2026-09-12 16:25:44 +0300

    Update death sound to classic arcade Game Over melody

--- cigar-client/assets/js/main_out.js
+++ cigar-client/assets/js/main_out.js
@@ -45,25 +45,34 @@
             osc.start(now);
             osc.stop(now + 0.4);
         } else if (type === 'death') {
-            // Deep descending resonant defeat tone
-            var osc1 = ctx.createOscillator();
-            var osc2 = ctx.createOscillator();
-            var gain = ctx.createGain();
-            osc1.type = 'sawtooth';
-            osc2.type = 'sine';
-            osc1.frequency.setValueAtTime(180, now);
-            osc1.frequency.exponentialRampToValueAtTime(45, now + 0.55);
-            osc2.frequency.setValueAtTime(90, now);
-            osc2.frequency.exponentialRampToValueAtTime(30, now + 0.6);
-            gain.gain.setValueAtTime(0.4, now);
-            gain.gain.exponentialRampToValueAtTime(0.001, now + 0.6);
-            osc1.connect(gain);
-            osc2.connect(gain);
-            gain.connect(masterGain);
-            osc1.start(now);
-            osc2.start(now);
-            osc1.stop(now + 0.62);
-            osc2.stop(now + 0.62);
+            // Iconic Arcade 8-bit "Game Over" 4-note descending melody (D4 -> C4 -> B3 -> G#3/G3)
+            var notes = [
+                { f: 293.66, t: 0.00, d: 0.16 }, // D4
+                { f: 261.63, t: 0.16, d: 0.16 }, // C4
+                { f: 246.94, t: 0.32, d: 0.16 }, // B3
+                { f: 196.00, t: 0.48, d: 0.45 }  // G3 (long resonant finish)
+            ];
+
+            notes.forEach(function(n) {
+                var osc = ctx.createOscillator();
+                var gain = ctx.createGain();
+                osc.type = 'triangle';
+                osc.frequency.setValueAtTime(n.f, now + n.t);
+                
+                // Add retro arcade slide down on the last note
+                if (n.t >= 0.48) {
+                    osc.frequency.exponentialRampToValueAtTime(140, now + n.t + n.d);
+                }
+
+                gain.gain.setValueAtTime(0.38, now + n.t);
+                gain.gain.exponentialRampToValueAtTime(0.001, now + n.t + n.d);
+
+                osc.connect(gain);
+                gain.connect(masterGain);
+
+                osc.start(now + n.t);
+                osc.stop(now + n.t + n.d + 0.02);
+            });
         } else if (type === 'virus') {
             // Sharp shattering noise burst + pop
             var osc = ctx.createOscillator();
