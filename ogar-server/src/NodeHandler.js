eb301ac555350b6a070a7a1c0aec96fabc39b039
BlobsIsrael <blobs@israel.io>
2026-09-13 19:24:30 +0300

    Align mass decay 1:1 with OgarII

--- ogar-server/src/NodeHandler.js
+++ ogar-server/src/NodeHandler.js
@@ -97,18 +97,9 @@ NodeHandler.prototype.update = function() {
             var t3e = process.hrtime(t3s),
                 t4s = process.hrtime();
 
-            // Mass decay with progressive scaling for large cells
+            // Mass decay (1:1 identical to OgarII formula)
             if (cell.mass >= this.gameServer.config.playerMinMassDecay) {
-                // Progressive mass decay scaling factor:
-                // Small cells (< 600) decay normally
-                // Medium/Large cells (1,000 - 50,000) experience significantly higher decay rate as they grow
-                var scaleFactor = 1.0;
-                if (cell.mass > 600) {
-                    scaleFactor = 1.0 + Math.pow(cell.mass / 1200, 0.85);
-                }
-                var effectiveDecayRate = (this.gameServer.config.playerMassDecayRate || 0.002) * scaleFactor;
-                var effectiveDecay = 1 - (effectiveDecayRate * this.gameServer.gameMode.decayMod / 25);
-                cell.mass *= effectiveDecay;
+                cell.mass *= thisDecay;
             }
 
             var t4e = process.hrtime(t4s);
